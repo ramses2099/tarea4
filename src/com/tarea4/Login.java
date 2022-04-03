638955ce0845272e9b4d98acef26a61c71bc36d8
ramses2099 <ramses2099@gmail.com>
2022-04-03 10:25:18 -0400

    first commit

---
+++ src/com/tarea4/Login.java
@@ -0,0 +1,116 @@
+package com.tarea4;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtNombreUsuario;
+	private JPasswordField passFContrasena;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 283, 350);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtNombreUsuario = new JTextField();
+		txtNombreUsuario.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtNombreUsuario.setBounds(10, 76, 247, 57);
+		contentPane.add(txtNombreUsuario);
+		txtNombreUsuario.setColumns(10);
+		
+		JLabel lblTitulo = new JLabel("Login");
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 40));
+		lblTitulo.setBounds(10, 13, 144, 52);
+		contentPane.add(lblTitulo);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 char[] input = passFContrasena.getPassword();
+			        if (isPasswordCorrect(input)) {
+			            JOptionPane.showMessageDialog(null,
+			                "Success! You typed the right password.");
+			        } else {
+			            JOptionPane.showMessageDialog(null,
+			                "Invalid password. Try again.",
+			                "Error Message",
+			                JOptionPane.ERROR_MESSAGE);
+			        }
+
+			        //Zero out the possible password, for security.
+			        Arrays.fill(input, '0');
+
+			        passFContrasena.selectAll();
+			        
+			}
+		});
+		btnEntrar.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnEntrar.setBounds(10, 212, 247, 37);
+		contentPane.add(btnEntrar);
+		
+		JButton btnRegistro = new JButton("Registrar");
+		btnRegistro.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnRegistro.setBounds(10, 260, 247, 37);
+		contentPane.add(btnRegistro);
+		
+		passFContrasena = new JPasswordField();
+		passFContrasena.setBounds(10, 144, 247, 57);
+		contentPane.add(passFContrasena);
+	}
+	
+	private static boolean isPasswordCorrect(char[] input) {
+	    boolean isCorrect = true;
+	    char[] correctPassword = { 'b', 'u', 'g', 'a', 'b', 'o', 'o' };
+
+	    if (input.length != correctPassword.length) {
+	        isCorrect = false;
+	    } else {
+	        isCorrect = Arrays.equals (input, correctPassword);
+	    }
+
+	    //Zero out the password.
+	    Arrays.fill(correctPassword,'0');
+
+	    return isCorrect;
+	}
+}
