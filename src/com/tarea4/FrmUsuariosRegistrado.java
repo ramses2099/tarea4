36da02304783603bcb219d977d91259e1332856a
navis service <navis.service@hitlab.local>
2022-04-04 18:38:13 -0400

    last commit

--- src/com/tarea4/FrmUsuariosRegistrado.java
+++ src/com/tarea4/FrmUsuariosRegistrado.java
@@ -6,16 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class FrmUsuariosRegistrado extends JFrame {
 
 	private JPanel contentPane;
+	private JTable tblUsuarios;
+	private DefaultTableModel model;
 
 	/**
 	 * Launch the application.
@@ -50,20 +54,32 @@ public class FrmUsuariosRegistrado extends JFrame {
 		contentPane.add(btnNuevo);
 		
 		JButton btnActualizar = new JButton("ACTUALIZAR");
-		btnActualizar.setBounds(136, 282, 106, 50);
+		btnActualizar.setBounds(126, 282, 106, 50);
 		contentPane.add(btnActualizar);
 		
 		JButton btnEliminar = new JButton("ELIMINAR");
-		btnEliminar.setBounds(252, 282, 106, 50);
+		btnEliminar.setBounds(242, 282, 106, 50);
 		contentPane.add(btnEliminar);
 		
 		JButton btnCerrarSeccion = new JButton("CERRAR SECCION");
-		btnCerrarSeccion.setBounds(368, 282, 127, 50);
+		btnCerrarSeccion.setBounds(358, 282, 137, 50);
 		contentPane.add(btnCerrarSeccion);
 		
 		JLabel lblNewLabel = new JLabel("Usuarios Registrado");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lblNewLabel.setBounds(10, 11, 205, 24);
 		contentPane.add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 46, 483, 222);
+		contentPane.add(scrollPane);
+		
+		tblUsuarios = new JTable();
+		model = new DefaultTableModel();
+		Object[] column = {"Nombre","Apellido","Telefono","Email","Usuario"};
+		Object[] row = new Object[0];
+		model.setColumnIdentifiers(column);
+		tblUsuarios.setModel(model);
+		scrollPane.setViewportView(tblUsuarios);
 	}
 }
